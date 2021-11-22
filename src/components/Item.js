fc36242a759fb444540a1cb595653b6741f56980
m-fidow <muna.a.fidow@gmail.com>
2021-11-22 22:17:36 +0000

    removed unused lines

--- src/components/Item.js
+++ src/components/Item.js
@@ -14,10 +14,6 @@ export default function Item({
   //   {item.map((x)=> console.log(x)}
   return (
     <>
-      {/* <TableCell component="th" scope="row">
-        <ProviderInfo {...item} />
-      </TableCell> */}
-
       <TableCell component="th" scope="row">
         <ProviderInfo {...item} />
       </TableCell>
@@ -25,42 +21,6 @@ export default function Item({
       <TableCell align="right">{internet_speed}</TableCell>
       <TableCell align="right">{set_up_cost}</TableCell>
       <TableCell align="right">{contract_info}</TableCell>
-
-      {/* {console.log(provider_name)} */}
-      {/* <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          {" "}
-          <TableBody>
-            {item.map((row) => (
-              <TableRow
-                key={row.provider_name}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  <Grid container>
-                   <Grid item><img src={} alt=""/></Grid>
-                    <Grid item><Box>{row.provider_name}</Box></Grid>
-                  </Grid>
-                </TableCell>
-                <TableCell align="right">{row.monthly_price}</TableCell>
-                <TableCell align="right">{row.internet_speed}</TableCell>
-                <TableCell align="right">{row.set_up_cost}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer> */}
-      {/* Hello{" "}
-      {item.map((x, index) => {
-        return (
-          <div>
-            <p>{x.provider_name}</p>
-            <img src={x.provider_logo_image_url} />
-          </div>
-        );
-      })}
-      {console.log({ item })} */}
     </>
   );
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -2,13 +2,12 @@ import React from "react";
 import Item from "./Item";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
+
 import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
+
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
+
 function DataTable({ items }) {
   return (
     <TableContainer component={Paper}>
@@ -19,16 +18,7 @@ function DataTable({ items }) {
               key={index}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
-              {/* <TableCell> */}
               <Item key={index} {...item} />
-              {/* </TableCell> */}
-              {/* <TableCell component="th" scope="row">
-                  <Grid container>
-                   <Grid item><img src={} alt=""/></Grid>
-                    <Grid item><Box>{row.provider_name}</Box></Grid>
-                  </Grid>
-                </TableCell>
-                <TableCell align="right">{row.monthly_price}</TableCell> */}
             </TableRow>
           ))}
         </TableBody>
