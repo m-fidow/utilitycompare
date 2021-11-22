2301cb4814a6845e023aeac42acb1a3e94a43ec0
m-fidow <muna.a.fidow@gmail.com>
2021-11-22 23:13:51 +0000

    star rating added circle version

--- src/components/ProviderInfo.js
+++ src/components/ProviderInfo.js
@@ -1,5 +1,10 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
+import Rating from "react-rating";
+import { Box } from "@mui/system";
+import { FaRegStar } from "react-icons/fa";
+import { FaStar } from "react-icons/fa/";
+
 function ProviderInfo({
   provider_name,
   provider_logo_image_url,
@@ -14,7 +19,15 @@ function ProviderInfo({
       <Grid item>
         {provider_name}
         <p>{deal_name}</p>
-        <p>{provider_rating}</p>
+        <Box>
+          <Rating initialRating={provider_rating * 5} readonly />
+        </Box>
+
+        {/* <Rating
+          emptySymbol="fa fa-star-o"
+          fullSymbol="fa fa-star"
+          fractions={2}
+        /> */}
       </Grid>
     </Grid>
   );
