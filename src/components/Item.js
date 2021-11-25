a2edac0ad0b74cd75521a557f5d9d942760ff8ac
m-fidow <muna.a.fidow@gmail.com>
2021-11-25 20:02:12 +0000

    remove button now toggling

--- src/components/Item.js
+++ src/components/Item.js
@@ -7,55 +7,54 @@ import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 
 export default function Item({
-  monthly_price,
-  internet_speed,
-  set_up_cost,
-  broadband_type,
-  contract_info,
-  ...item
+  item,
+  // monthly_price,
+  // internet_speed,
+  // set_up_cost,
+  // broadband_type,
+  // contract_info,
+  addToCompare,
+  removeFromCompare,
+  comparedItems,
+  // deal_id,
 }) {
-  const [open, setOpen] = useState(false);
-  const [comparedItems, setComparedItems] = useState([]);
-  const toggleDrawer = (product) => {
-    setOpen(true);
-    addToCompare(product);
-    console.log(open);
-  };
-  const addToCompare = (product) => {
-    setComparedItems((comparedItems) => [...comparedItems, product]);
-    console.log(comparedItems);
-  };
   return (
     <>
       <TableCell component="th" scope="row">
         <ProviderInfo {...item} />
       </TableCell>
       <TableCell align="right">
-        £{monthly_price}
+        £{item.monthly_price}
         <Grid item>Monthy Cost</Grid>
       </TableCell>
       <TableCell align="right">
         <Grid>
-          <Grid item>{internet_speed} Mbps</Grid>
-          <Grid item>{broadband_type} Speed</Grid>
+          <Grid item>{item.internet_speed} Mbps</Grid>
+          <Grid item>{item.broadband_type} Speed</Grid>
         </Grid>
       </TableCell>
       <TableCell align="right">
-        <Grid>£{set_up_cost}</Grid>
+        <Grid>£{item.set_up_cost}</Grid>
         <Grid item>Setup Costs</Grid>
       </TableCell>
       <TableCell align="right">
         <Grid>
-          <Grid item> {contract_info}</Grid>
+          <Grid item> {item.contract_info}</Grid>
           <Grid item>Contract</Grid>
         </Grid>
       </TableCell>
       <TableCell align="center">
-        <Button variant="contained" onClick={() => toggleDrawer(item)}>
-          Add to compare
-        </Button>
+        {comparedItems && comparedItems.includes(item) ? (
+          <Button variant="contained" onClick={() => removeFromCompare(item)}>
+            remove from compare
+          </Button>
+        ) : (
+          <Button variant="contained" onClick={() => addToCompare(item)}>
+            Add to compare
+          </Button>
+        )}
       </TableCell>
-      <Drawer state={open} />
     </>
   );
 }
+// onClick={() => toggleDrawer(item)}
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
+import Drawer from "./Drawer";
 // import Button from "@mui/material/Button";
 
 import TableContainer from "@mui/material/TableContainer";
@@ -10,20 +11,53 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
 function DataTable({ items }) {
+  // {
+  //   console.log({ items });
+  // }
+  const [open, setOpen] = useState(true);
+  const [comparedItems, setComparedItems] = useState([]);
+  // const toggleDrawer = (product) => {
+  //   // addToCompare(product);
+  //   setOpen(true);
+
+  //   console.log(open);
+  // };
+  const addToCompare = (x) => {
+    setComparedItems((comparedItems) => [...comparedItems, x]);
+    console.log(x.provider_name);
+    console.log({ comparedItems });
+    console.log(comparedItems.length);
+  };
+  const removeFromCompare = (x) => {
+    const filteredItems = comparedItems.filter(
+      (product) => product.deal_id !== x.deal_id
+    );
+    console.log({ filteredItems });
+    setComparedItems(() => filteredItems);
+    // console.log({ filteredItems });
+    console.log(x.deal_id);
+  };
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableBody>
-          {items.map((item, index) => (
+          {items.map((item) => (
             <TableRow
-              key={index}
+              key={item.deal_id}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
-              <Item key={index} {...item} />
+              <Item
+                key={item.deal_id}
+                comparedItems={comparedItems}
+                addToCompare={addToCompare}
+                removeFromCompare={removeFromCompare}
+                item={item}
+              />
             </TableRow>
           ))}
         </TableBody>
       </Table>
+      <Drawer state={open} />
     </TableContainer>
   );
 }
