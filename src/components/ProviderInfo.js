048afdb0c449431074dccc7ddd7a1cc71684e34c
m-fidow <muna.a.fidow@gmail.com>
2021-11-26 12:32:14 +0000

    styled provider info

--- src/components/ProviderInfo.js
+++ src/components/ProviderInfo.js
@@ -2,7 +2,7 @@ import React from "react";
 import Grid from "@mui/material/Grid";
 // import Rating from "react-rating";
 import Stack from "@mui/material/Stack";
-
+import { Typography } from "@mui/material";
 function ProviderInfo({
   provider_name,
   provider_logo_image_url,
@@ -11,13 +11,17 @@ function ProviderInfo({
   rating,
 }) {
   return (
-    <Grid container>
+    <Grid container spacing={1}>
       <Grid item>
         <img style={{ width: "70px" }} src={provider_logo_image_url} alt="" />
       </Grid>
       <Grid item>
-        {provider_name}
-        <p>{deal_name}</p>
+        <Typography component="h1" variant="h6">
+          {provider_name}
+        </Typography>
+        <Typography component="h2" variant="caption">
+          {deal_name}
+        </Typography>
         <Stack spacing={1}>{rating}</Stack>
       </Grid>
     </Grid>
